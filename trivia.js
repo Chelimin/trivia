53b9adfafff55bc678cd5f63c2edfc2a1b751de2
He Limin <chelimin@gmail.com>
2016-03-02 23:30:16 +0800

    kinda working

--- trivia.js
+++ trivia.js
@@ -1,117 +1,119 @@
+var seconds = 20;
 var count=0;
-var qn = document.getElementById('qn');
-var ans = document.getElementById('ans');
-var score = document.getElementById('score');
-var img1 = document.getElementById('img1');
-var img2 = document.getElementById('img2');
-var img3 = document.getElementById('img3');
-var img4 = document.getElementById('img4');
-var img5 = document.getElementById('img5');
-var img6 = document.getElementById('img6');
-var img7 = document.getElementById('img7');
+var counter = 0;
+var input = document.getElementById('input');
 var q1 = "What is the population of Singapore to the nearest million?";
 var q2 = "How many islands is Singapore made up of?";
 var q3 = "How many Prime Ministers did Singapore have, including the current one?";
 var q4 = "How many Presidents did Singapore have?";
 var q5 = "How many MRT stations are operating in Singapore?";
-
-function qn1(){
-qn.textContent = q1;
-var input1 = parseInt(prompt(q1));
-while (input1 !== 5){
-  if(input1 < 5){
-    input1 = parseInt(prompt('Thats not it, try going higher?'));
-  } else if (input1>5){
-    input1 = parseInt(prompt('Thats not it, try going lower?'));
-  }
-}
-count++
-ans.textContent='5 million';
-score.textContent = "You have " + count + "/5 questions correct!"
-ans.className = "right";
-lily1.className = "show";
-}
-
-function qn2(){
-qn.textContent = q1;
-var input2 = parseInt(prompt(q2));
-while (input2 !== 63){
-  if(input2 < 63){
-    input2 = parseInt(prompt('Thats not it, try going higher?'));
-  } else if (input2>63){
-    input2 = parseInt(prompt('Thats not it, try going lower?'));
-  }
-}
-count++
-ans.textContent='63 islands';
-score.textContent = "You have " + count + "/5 questions correct!"
-ans.className = "right";
-lily2.className = "show";
-}
-
-function qn3(){
-qn.textContent = q2;
-var input3 = parseInt(prompt(q3));
-while (input3 !== 3){
-  if(input3 < 3){
-    input3 = parseInt(prompt('Thats not it, try going higher?'));
-  } else if (input3 > 3){
-    input3 = parseInt(prompt('Thats not it, try going lower?'));
-  }
-}
-count++
-ans.textContent='3 Prime Ministers';
-score.textContent = "You have " + count + "/5 questions correct!"
-ans.className = "right";
-lily3.className = "show";
-}
-
-function qn4(){
-qn.textContent = q3;
-var input4 = parseInt(prompt(q4));
-while (input4 !== 7){
-  if(input4 < 7){
-    input4 = parseInt(prompt('Thats not it, try going higher?'));
-  } else if (input4>7){
-    input4 = parseInt(prompt('Thats not it, try going lower?'));
-  }
-}
-count++
-ans.textContent='7 Presidents';
-score.textContent = "You have " + count + "/5 questions correct!"
-ans.className = "right";
-lily4.className = "show";
-}
-
-function qn5(){
-qn.textContent = q4;
-var input5 = parseInt(prompt(q5));
-while (input5 !== 109){
-  if(input5 < 109){
-    input5 = parseInt(prompt('Thats not it, try going higher?'));
-  } else if (input5>109){
-    input5 = parseInt(prompt('Thats not it, try going lower?'));
-  }
-}
-count++
-ans.textContent='109 stations';
-score.textContent = "You have " + count + "/5 questions correct!"
-ans.className = "right";
-lily5.className = "show";
-}
-
+var qnArray = [q1,q2,q3,q4,q5]
+var ansArray = [5,63,3,7,109]
+var newGame = new Game(qnArray, ansArray)
 frog.hidden = false;
 frog.className = "show";
 snake.className = "show";
-qn1();
-qn2();
-qn3();
-qn4();
-qn5();
-qn.textContent = q5;
-if (count=5){
-  snake.className="show";
-  frog.hidden = true;
-  victoryfrog.hidden = false;
-  victoryfrog.className = "show";
-}
+
+function Game(qn,ans){
+  this.qn = qn;
+  this.ans = ans;
+  this.play = function() {
+    // question.textContent = this.qn[counter];
+    $('#question').text(this.qn[counter]);
+  }
+  this.checkAns = function() {
+    response = parseInt(input.value);
+    if (isNaN(response) ==false){
+     if (response < this.ans[counter]){
+       $('#hint').text('Thats not it, try going HIGHER?');
+       $('#input').val('');
+        }
+     else if (response > this.ans[counter]){
+       $('#hint').text('Thats not it, try going LOWER?');
+       $('#input').val('');
+        }
+     else {
+       count++;
+       counter++;
+       $('#input').val('');
+       $('#input').focus();
+       $('#hint').text('');
+      //  $('#ans').text(this.ans[counter]);
+       $('#score').text("You have " + count + "/5 questions correct!");
+       var lily = "lily" + counter;
+       $("#" + lily).attr('class', 'show')
+       //document.getElementById(lily).className = "show";
+       this.play();
+        }
+      }else(alert("Please enter a valid input!"));
+    this.endGame = function () {
+      frog.hidden=true;
+      victoryfrog.hidden=false;
+      $('#question').text('YOU SAVED FROGGY!!!');
+      $('#victoryfrog').attr('class','show');
+      clearInterval(intervalId);
+      $('#time').html('');
+      $('#start').show();
+      counter = 0;
+      count = 0;
+    }
+    if (counter===5){
+      this.endGame();
+    }
+  }
+};
+
+$("#input").on("keydown", function (e) {
+    if (e.keyCode === 13) {
+        newGame.checkAns(e);
+    }
+});
+
+//start button
+
+var start = $('#start');
+var intervalId;
+
+
+start.on('click',function () {
+  for (i=1; i<qnArray.length+1;i++){
+    $('#lily' + [i]).attr('class', 'hide');
+  }
+  frog.hidden = false;
+  frog.className = "show";
+  snake.className = "show";
+  victoryfrog.hidden=true;
+  newGame.play()
+  localStorage.clear();
+  count=0;
+  counter=0;
+  $('#start').hide();
+  $('#input').focus();
+  $('#score').text(" ");
+  intervalId = setInterval (function () {
+      updateTime()
+  }, 1000 );
+})
+
+  function updateTime() {
+    seconds --;
+    $('#time').html('Time remaining: ' + seconds +'s');
+    if(seconds <= 0){
+        clearInterval(intervalId);
+        seconds = 20;
+        $('#time').html('');
+        $('#start').show();
+        $('#question').text("TIME'\S UP !!! ");
+      }
+    }
+
+    // function checkInput(x){
+    // if ((x%1) != 0 || x!==" "){
+    //     alert("Please don't try to be funny and enter valid input!");
+    //     return false;
+    //   }
+    // }
+
+  //   $('#start').on('click',function() {
+  //  location.reload();
+  //    });
