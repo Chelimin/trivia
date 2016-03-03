9cc4fb4c4161ba4c75b382757536024459c5dd82
He Limin <chelimin@gmail.com>
2016-03-03 22:45:18 +0800

    more css changes

--- trivia.js
+++ trivia.js
@@ -1,24 +1,24 @@
-var seconds = 20;
 var count=0;
 var counter = 0;
+var seconds = 10;
 var input = document.getElementById('input');
 var q1 = "What is the population of Singapore to the nearest million?";
 var q2 = "How many islands is Singapore made up of?";
 var q3 = "How many Prime Ministers did Singapore have, including the current one?";
-var q4 = "How many Presidents did Singapore have?";
+var q4 = "How many Presidents did Singapore have, including the current one?";
 var q5 = "How many MRT stations are operating in Singapore?";
 var qnArray = [q1,q2,q3,q4,q5]
 var ansArray = [5,63,3,7,109]
-var newGame = new Game(qnArray, ansArray)
 frog.hidden = false;
 frog.className = "show";
 snake.className = "show";
+
+var newGame = new Game(qnArray, ansArray)
 
 function Game(qn,ans){
   this.qn = qn;
   this.ans = ans;
   this.play = function() {
-    // question.textContent = this.qn[counter];
     $('#question').text(this.qn[counter]);
   }
   this.checkAns = function() {
@@ -38,16 +38,16 @@ function Game(qn,ans){
        $('#input').val('');
        $('#input').focus();
        $('#hint').text('');
-      //  $('#ans').text(this.ans[counter]);
        $('#score').text("You have " + count + "/5 questions correct!");
        var lily = "lily" + counter;
        $("#" + lily).attr('class', 'show')
        //document.getElementById(lily).className = "show";
        this.play();
         }
-      }else(alert("Please enter a valid input!"));
+      }else(alert("Trying to be funny?"));
     this.endGame = function () {
       frog.hidden=true;
+      snake.hidden=true;
       victoryfrog.hidden=false;
       $('#question').text('YOU SAVED FROGGY!!!');
       $('#victoryfrog').attr('class','show');
@@ -79,14 +79,18 @@ start.on('click',function () {
   for (i=1; i<qnArray.length+1;i++){
     $('#lily' + [i]).attr('class', 'hide');
   }
+  $('#snake').attr("src", "images/snake.png");
+  $('#frog').attr("src","images/frog.png")
+  title.hidden = true;
   frog.hidden = false;
   frog.className = "show";
   snake.className = "show";
-  victoryfrog.hidden=true;
-  newGame.play()
-  localStorage.clear();
+  victoryfrog.className="hide";
   count=0;
   counter=0;
+  console.log(count);
+  console.log(counter);
+  newGame.play();
   $('#start').hide();
   $('#input').focus();
   $('#score').text(" ");
@@ -100,10 +104,12 @@ start.on('click',function () {
     $('#time').html('Time remaining: ' + seconds +'s');
     if(seconds <= 0){
         clearInterval(intervalId);
-        seconds = 20;
+        seconds = 10;
         $('#time').html('');
         $('#start').show();
         $('#question').text("TIME'\S UP !!! ");
+        $('#snake').attr("src", "images/burp.jpg");
+        $('#frog').attr("src","images/snakeeat.jpg")
       }
     }
 
